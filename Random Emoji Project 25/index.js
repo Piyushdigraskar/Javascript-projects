d832a2614a8c972f70db9f1a0047e2f850d17d2a
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-06-28 10:46:48 +0530

    To be deployed

---
+++ Random Emoji Project 25/index.js
@@ -0,0 +1,27 @@
+ const btnEl = document.getElementById('btn');
+
+ const emojinameEl = document.getElementById('emoji-name')
+
+const emoji = [];
+
+async function getEmoji(){
+    let response = await fetch("https://emoji-api.com/emojis?access_key=8a170e9d1010f5e249393a7cda7e64490fb4e10b");
+
+    data = await response.json();
+    console.log(data);
+    for(let i=0;i<1500;i++){
+        emoji.push({
+            emojiName:data[i].character,
+            emojCode:data[i].unicodeName,
+        })
+    }
+}
+
+getEmoji();
+
+ btnEl.addEventListener('click', ()=>{
+    const randomNum = Math.floor(Math.random()* emoji.length)
+
+    btnEl.innerText = emoji[randomNum].emojiName;
+    emojinameEl.innerText = emoji[randomNum].emojicode;
+ })
